b6cd66c6cdf1d001f1ade3113e6d4e3a645a649e
saaransht <tansh2612@gmail.com>
2024-06-26 12:19:32 +0530

    Project Set-up

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,31 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const {mainRouter} = require('./routes/index')
+
+const cors = require('cors')
+
+const PORT = 3000 || process.env.PORT;
+
+mongoose.connect('mongodb+srv://admin:YsdgTkdqWdninDVD@cluster0.9pc7wem.mongodb.net/')
+.then(()=> console.log('DB CONNECTION SUCCESS'))
+.catch((err)=> console.log(err));
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/api/v1' , mainRouter);
+
+
+
+
+
+app.listen(PORT , ()=>{
+    console.log(`Server Running on 3000`);
+})
+
 
 
